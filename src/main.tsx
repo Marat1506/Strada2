bf72b6d198098b49133c60213281fac9419d1448
Marat <maratmirzabalaev@gmail.com>
2024-02-25 01:52:35 +0300

    set up routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Panel from './pages/panel/panel.tsx'
+import Payments from './pages/payments/payments.tsx'
+import Roadmaps from './pages/roadmaps/roadmaps.tsx'
+import Tasks from './pages/tasks/tasks.tsx'
+import Users from './pages/users/users.tsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/panel',
+    element: <Panel />
+  },
+  {
+    path: '/payments',
+    element: <Payments />
+  },
+  {
+    path: '/roadmaps',
+    element: <Roadmaps />
+  },
+  {
+    path: '/tasks',
+    element: <Tasks />
+  },
+  {
+    path: '/users',
+    element: <Users />
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ src/pages/roadmaps/roadmaps.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Roadmaps() {
+  return (
+    <div>roadmaps</div>
+  )
+}
